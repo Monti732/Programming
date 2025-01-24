4047f98830c533bb19914d074c2914945068f763
Monti732 <viktorsmakov073@gmail.com>
2025-01-24 21:19:43 +0700

    fixed 16 answer

--- answers/first semester/16/16.cpp
+++ answers/first semester/16/16.cpp
@@ -3,21 +3,36 @@
 #include <algorithm>
 
 int main() {
-    std::vector<std::vector<int>> array = {{1,2,3,4,5}, {6,7,8,9,1}, {2,3,4,5,6}, {7,8,9,1,2}, {3,4,5,6,7}};
-    for (int i = 0; i < 5; ++i) {
-        for (int j = i + 1; j < 5; ++j) {
+    std::vector<std::vector<int>> array;
+    int numberOfLines, numberOfColumns;
+    std::cout << "Enter number of lines ";
+    std::cin >> numberOfLines;
+    std::cout << "Enter number of columns ";
+    std::cin >> numberOfColumns;
+    std::cout << "Enter elements ";
+    for (int i = 0; i < numberOfLines; ++i) {
+        std::vector<int> line;
+        for (int j = 0; j < numberOfColumns; ++j) {
+            int element;
+            std::cin >> element;
+            line.push_back(element);
+        }
+        array.push_back(line);
+    }
+    for (int i = 0; i < numberOfLines; ++i) {
+        for (int j = i + 1; j < numberOfColumns; ++j) {
             std::swap(array[i][j], array[j][i]);
         }
     }
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < numberOfLines; ++i) {
         std::reverse(array[i].begin(), array[i].end());
     }
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < numberOfLines; ++i) {
         int midSumInColomn = 0;
-        for (int j = 0; j < 5;++j) {
+        for (int j = 0; j < numberOfColumns;++j) {
             midSumInColomn += array[i][j];
         }
-        std::cout << "mid sum in colomn " << i + 1 << "= " << midSumInColomn / (5 * 1.0) << '\n';
+        std::cout << "mid sum in colomn " << i + 1 << " = " << midSumInColomn / (numberOfColumns * 1.0) << '\n';
     }
 
 }
