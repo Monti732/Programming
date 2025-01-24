1ffce9dda7a408b295df17ba3d4a0176022e0a5b
Monti732 <viktorsmakov073@gmail.com>
2025-01-24 21:14:30 +0700

    fixed 14 answer

--- answers/first semester/14/14.cpp
+++ answers/first semester/14/14.cpp
@@ -3,28 +3,37 @@
 #include <algorithm>
 
 int main() {
-    std::vector<std::vector<int>> array ={
-        {-1, -2, 3, -4, 5},
-        {6, 7, -8, 9, -10},
-        {-11, 12, -13, -14, -15},
-        {16, -17, 18, 19, 20},
-        {-21, -22, -23, -24, -25}
-    };
-    for (int i = 0; i < 5; ++i) {
-        for (int j = i + 1; j < 5; ++j) {
+    std::vector<std::vector<int>> array;
+    int numberOfLines, numberOfColumns;
+    std::cout << "Enter number of lines ";
+    std::cin >> numberOfLines;
+    std::cout << "Enter number of columns ";
+    std::cin >> numberOfColumns;
+    std::cout << "Enter elements ";
+    for (int i = 0; i < numberOfLines; ++i) {
+        std::vector<int> line;
+        for (int j = 0; j < numberOfColumns; ++j) {
+            int element;
+            std::cin >> element;
+            line.push_back(element);
+        }
+        array.push_back(line);
+    }
+    for (int i = 0; i < numberOfLines; ++i) {
+        for (int j = i + 1; j < numberOfColumns; ++j) {
             std::swap(array[i][j], array[j][i]);
         }
     }
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < numberOfLines; ++i) {
         std::reverse(array[i].begin(), array[i].end());
     }
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < numberOfLines; ++i) {
         int sum = 0;
-        for (int j = 0; j < 5; ++j) {
+        for (int j = 0; j < numberOfColumns; ++j) {
             if (array[i][j] < 0) {
                 sum += array[i][j];
             }
         }
-        std::cout << "sum of neg nums in colomn " << i + 1 << "= " << sum << '\n';
+        std::cout << "sum of neg nums in colomn " << i + 1 << " = " << sum << '\n';
     }
 }
