feaa28abdf8b2991e43a6f0183cc945afd451e5e
Monti732 <viktorsmakov073@gmail.com>
2025-01-24 22:18:31 +0700

    fixed 17 answer

---
+++ answers/first semester/17/17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int numberOfFloors, numberOfFlatsOnFloor, numberOfFlat;
+    std::cout << "Enter number of floors ";
+    std::cin >> numberOfFloors;
+    std::cout << "Enter number of flats on floor ";
+    std::cin >> numberOfFlatsOnFloor;
+    std::cout << "Enter number of flat ";
+    std::cin >> numberOfFlat;
+    if ((numberOfFlat / numberOfFlatsOnFloor) % 2 == 0) {
+        std::cout << "Elevator will stop on " << numberOfFlat / numberOfFlatsOnFloor - 1 << " floor";
+    }
+    else {
+        std::cout << "Elevator will stop on " << numberOfFlat / numberOfFlatsOnFloor << " floor";
+    }
+
+}
