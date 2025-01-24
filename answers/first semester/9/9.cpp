79410cb8babbe0ea17e676e78d9cccf34e74b36c
Monti732 <viktorsmakov073@gmail.com>
2025-01-24 19:44:50 +0700

    fixed 9 answer

--- answers/first semester/9/9.cpp
+++ answers/first semester/9/9.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    int array[5] = {7,3,8,9,4};
-    for (int i = 0; i< 5; i++) {
-        for (int j = 0; j < 5 - i - 1; ++j) {
+    std::vector<int> array;
+    int numberOfElements;
+    std::cout << "Enter number of elements ";
+    std::cin >> numberOfElements;
+    std::cout << "Enter elements ";
+    for (int i = 0; i < numberOfElements; ++i) {
+        int element;
+        std::cin >> element;
+        array.push_back(element);
+    }
+    for (int i = 0; i < numberOfElements; i++) {
+        for (int j = 0; j < numberOfElements - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
                 std::swap(array[j], array[j + 1]);
             }
         }
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < numberOfElements; i++) {
         std::cout << array[i] << " ";
     }
 }
