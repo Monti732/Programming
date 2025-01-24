cf55f8b22ef137c848724d62d6a2d0d38ff5a4a9
Monti732 <viktorsmakov073@gmail.com>
2025-01-24 19:50:53 +0700

    fixed 10 answer

--- answers/first semester/10/10.cpp
+++ answers/first semester/10/10.cpp
@@ -2,12 +2,13 @@
 
 int main() {
     int x1, y1, x2, y2, x3, y3, x4, y4;
+    std::cout << "Enter coordinates of points ";
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-    int mid1_x = (x1 + x3) / 2;
-    int mid1_y = (y1 + y3) / 2;
-    int mid2_x = (x2 + x4) / 2;
-    int mid2_y = (y2 + y4) / 2;
-    if ((mid1_x == mid2_x) && (mid1_y == mid2_y)) {
+    int mid1X = (x1 + x3) / 2;
+    int mid1Y = (y1 + y3) / 2;
+    int mid2X = (x2 + x4) / 2;
+    int mid2Y = (y2 + y4) / 2;
+    if ((mid1X == mid2X) && (mid1Y == mid2Y)) {
         std::cout << "YES";
     }
     else {
