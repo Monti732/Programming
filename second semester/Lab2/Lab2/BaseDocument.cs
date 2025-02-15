3a47da932cf3cba2286dc3c23bc7368d2e422b4e
Monti732 <viktorsmakov073@gmail.com>
2025-02-15 11:10:21 +0700

    add menu class, main program and base for document class

---
+++ second semester/Lab2/Lab2/BaseDocument.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab2;
+
+public class BaseDocument {
+  private string _name;
+  private string _author;
+  private string[] _keywords;
+  private string _topic;
+  private string _path;
+
+  public void GetInformation(string _name, string _author, string _topic, string _path, string[] _keywords) {
+    
+  }
+}
---
+++ second semester/Lab2/Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab2;
+
+class Program() {
+  static void Main() {
+    string[] menuItens = {
+      "MS Word",
+      "PDF",
+      "MS Excel",
+      "TXT",
+      "HTML",
+      "Exit program"
+    };
+
+    Menu menu = new Menu(menuItens);
+
+    while (true) {
+      menu.Show();
+      int choce = menu.GetSelectedIndex();
+
+      
+    }
+  }
+}
