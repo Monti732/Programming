a990ec0d795cba31113f1c4abbabcb17bfba681b
Monti732 <viktorsmakov073@gmail.com>
2025-03-14 03:31:26 +0700

    add proto pattern(deepcopy), inverse method, true/false and explicit to str convert

--- second semester/Lab3/Lab3/Program.cs
+++ second semester/Lab3/Lab3/Program.cs
@@ -1,7 +1,16 @@
-﻿using Lab3;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Lab3;
 
 class Program() {
   static void Main() {
-    
+    string[] menuItems = { "Summarize matrices", "Multiply matrices", "Compare matrices", "Convert to string", };
+    SquareMatrix a = new SquareMatrix(3, -5, 5, false);
+    SquareMatrix b = new SquareMatrix(3, -5, 5, false);
+    Console.WriteLine(a.ToString());
+    Console.WriteLine(b.ToString());
+    Console.WriteLine(a+b);
+    Console.WriteLine(a*b);
+    Console.WriteLine(a.Determinant());
   }
 }
--- second semester/Lab3/Lab3/SquareMatrix.cs
+++ second semester/Lab3/Lab3/SquareMatrix.cs
@@ -2,107 +2,201 @@
 
 public class SquareMatrix : ICloneable, IComparable<SquareMatrix> {
   private readonly int _size;
-  private double[,] _matrix;
+  private int[,] _matrix;
 
   public int Size => _size;
-  public double[,] Matrix => _matrix;
 
-  // 🔹 Конструктор: Создание случайной матрицы
-  public SquareMatrix(int size, int minValue, int maxValue) {
-    if (size <= 0) throw new MatrixException("Размер матрицы должен быть больше нуля!");
+/*Если нам нужно реализовать матричный калькулятор то зачем нужен конструктор для ее случайной генерации?*/
+  public SquareMatrix(int size, int minValue, int maxValue, bool fillByYourself) {
+    if (size <= 0) throw new MatrixException("Matrix size must be greater than 0!");
 
     _size = size;
-    _matrix = new double[size, size];
+    _matrix = new int[size, size];
 
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
-    for (int j = 0; j < size; j++)
-      _matrix[i, j] = rnd.Next(minValue, maxValue);
+    if (fillByYourself) {
+      for (int i = 0; i < _size; ++i) {
+        for (int j = 0; j < _size; ++j) {
+          while (true) {
+            try {
+              Console.Clear();
+              Console.WriteLine("Enter elements of the matrix:");
+
+              Console.Write($"Element [{i},{j}]: ");
+              int value = int.Parse(Console.ReadLine());
+
+              if (value > maxValue) {
+                throw new ArgumentOutOfRangeException($"Value {value} is too large! Max allowed: {maxValue}");
+              }
+
+              _matrix[i, j] = value;
+
+              break;
+            }
+            catch (FormatException) {
+              Console.WriteLine("Invalid input! Please enter a valid number.");
+              Console.ReadKey();
+            }
+            catch (ArgumentOutOfRangeException ex) {
+              Console.WriteLine($"Error: {ex.Message}");
+              Console.ReadKey();
+            }
+          }
+        }
+      }
+    }
+
+    else {
+      Random rnd = new Random();
+      for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+          _matrix[i, j] = rnd.Next(minValue, maxValue);
+        }
+      }
+    }
   }
 
   public SquareMatrix(int size) {
     _size = size;
-    _matrix = new double[size, size];
+    _matrix = new int[size, size];
   }
-  
-  // 🔹 Конструктор копирования (Прототип)
+
   public SquareMatrix(SquareMatrix other) {
     _size = other._size;
-    _matrix = (double[,])other._matrix.Clone();
+    _matrix = DeepCopy(other._matrix);
   }
 
-  // 🔹 Перегрузка оператора "+"
-  public static SquareMatrix operator +(SquareMatrix a, SquareMatrix b) {
-    if (a.Size != b.Size) throw new MatrixException("Размеры матриц должны совпадать!");
+  private static int[,] DeepCopy(int[,] source) {
+    int rows = source.GetLength(0);
+    int cols = source.GetLength(1);
+    int[,] copy = new int[rows, cols];
 
-    SquareMatrix result = new SquareMatrix(a.Size);
-    for (int i = 0; i < a.Size; i++)
-    for (int j = 0; j < a.Size; j++)
-      result._matrix[i, j] = a._matrix[i, j] + b._matrix[i, j];
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        copy[i, j] = source[i, j];
+      }
+    }
 
-    return result;
+    return copy;
   }
 
-  // 🔹 Перегрузка оператора "*"
-  public static SquareMatrix operator *(SquareMatrix a, SquareMatrix b) {
-    if (a.Size != b.Size) throw new MatrixException("Размеры матриц должны совпадать!");
+  public object Clone() => new SquareMatrix(this);
 
-    SquareMatrix result = new SquareMatrix(a.Size);
-    for (int i = 0; i < a.Size; i++)
-    for (int j = 0; j < a.Size; j++)
-    for (int k = 0; k < a.Size; k++)
-      result._matrix[i, j] += a._matrix[i, k] * b._matrix[k, j];
-
-    return result;
-  }
-
-  // 🔹 Нахождение детерминанта (рекурсивный метод)
-  public double Determinant() {
+  /*I don't know why this...thing don't work, but I'd really like to find out*/
+  public int Determinant() {
     if (_size == 1) return _matrix[0, 0];
 
-    double det = 0;
-    for (int i = 0; i < _size; i++) {
-      SquareMatrix minor = GetMinor(this, i);
+    int det = 0;
+    for (int i = 0; i < _size; ++i) {
+      SquareMatrix minor = GetMinor(this, i, i);
       det += ((i % 2 == 0) ? 1 : -1) * _matrix[0, i] * minor.Determinant();
     }
 
     return det;
   }
 
-  // 🔹 Получение минора для детерминанта
-  private static SquareMatrix GetMinor(SquareMatrix matrix, int col) {
-    SquareMatrix minor = new SquareMatrix(matrix._size - 1);
-    for (int i = 1; i < matrix._size; i++)
-    for (int j = 0, minorJ = 0; j < matrix._size; j++)
-      if (j != col)
-        minor._matrix[i - 1, minorJ++] = matrix._matrix[i, j];
+  public SquareMatrix GetMinor(SquareMatrix matrix, int row, int col) {
+    int size = matrix.Size;
+    SquareMatrix minor = new SquareMatrix(size - 1);
+
+    for (int i = 0, minorI = 0; i < size; i++) {
+      if (i == row) continue;
+      for (int j = 0, minorJ = 0; j < size; j++) {
+        if (j == col) continue;
+        minor._matrix[minorI, minorJ] = matrix._matrix[i, j];
+        ++minorJ;
+      }
+
+      ++minorI;
+    }
 
     return minor;
   }
 
-  // 🔹 Перегрузка операторов сравнения (по детерминанту)
+  private SquareMatrix Adjoint() {
+    int size = this.Size;
+    SquareMatrix adjoint = new SquareMatrix(size);
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        SquareMatrix minor = GetMinor(this, i, j);
+        int minorDet = minor.Determinant();
+
+        int sign = (i + j) % 2 == 0 ? 1 : -1;
+
+        adjoint._matrix[j, i] = sign * minorDet;
+      }
+    }
+
+    return adjoint;
+  }
+
+  public SquareMatrix Inverse() {
+    int det = Determinant();
+    if (det == 0)
+      throw new MatrixException("Inverse matrix doesn't exist, determinant is zero!");
+
+    int size = this.Size;
+    SquareMatrix inverse = new SquareMatrix(size);
+    SquareMatrix adjoint = Adjoint();
+
+    for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      inverse._matrix[i, j] = adjoint._matrix[i, j] / det;
+
+    return inverse;
+  }
+
+  public static SquareMatrix operator +(SquareMatrix a, SquareMatrix b) {
+    if (a.Size != b.Size) throw new MatrixException("Matrix size mismatch!");
+
+    SquareMatrix result = new SquareMatrix(a.Size);
+    for (int i = 0; i < a.Size; ++i) {
+      for (int j = 0; j < a.Size; ++j) {
+        result._matrix[i, j] = a._matrix[i, j] + b._matrix[i, j];
+      }
+    }
+
+    return result;
+  }
+
+  public static SquareMatrix operator *(SquareMatrix a, SquareMatrix b) {
+    if (a.Size != b.Size) throw new MatrixException("Matrix size mismatch!");
+
+    SquareMatrix result = new SquareMatrix(a.Size);
+    for (int i = 0; i < a.Size; ++i) {
+      for (int j = 0; j < a.Size; ++j) {
+        for (int k = 0; k < a.Size; k++) {
+          result._matrix[i, j] += a._matrix[i, k] * b._matrix[k, j];
+        }
+      }
+    }
+
+    return result;
+  }
+
   public static bool operator >(SquareMatrix a, SquareMatrix b) => a.Determinant() > b.Determinant();
   public static bool operator <(SquareMatrix a, SquareMatrix b) => a.Determinant() < b.Determinant();
   public static bool operator >=(SquareMatrix a, SquareMatrix b) => a.Determinant() >= b.Determinant();
   public static bool operator <=(SquareMatrix a, SquareMatrix b) => a.Determinant() <= b.Determinant();
 
-  // 🔹 Операторы равенства и неравенства
   public static bool operator ==(SquareMatrix a, SquareMatrix b) {
     if (a.Size != b.Size) return false;
-    for (int i = 0; i < a.Size; i++)
-    for (int j = 0; j < a.Size; j++)
-      if (a._matrix[i, j] != b._matrix[i, j])
-        return false;
+    for (int i = 0; i < a.Size; ++i) {
+      for (int j = 0; j < a.Size; ++j) {
+        if (a._matrix[i, j] != b._matrix[i, j])
+          return false;
+      }
+    }
+
     return true;
   }
 
   public static bool operator !=(SquareMatrix a, SquareMatrix b) => !(a == b);
 
-  // 🔹 Преобразование в строку
   public override string ToString() {
     string result = "";
-    for (int i = 0; i < _size; i++) {
-      for (int j = 0; j < _size; j++)
+    for (int i = 0; i < _size; ++i) {
+      for (int j = 0; j < _size; ++j)
         result += $"{_matrix[i, j],6:F2} ";
       result += "\n";
     }
@@ -110,11 +204,38 @@ public class SquareMatrix : ICloneable, IComparable<SquareMatrix> {
     return result;
   }
 
-  // 🔹 Реализация интерфейсов
   public int CompareTo(SquareMatrix other) => Determinant().CompareTo(other.Determinant());
   public override bool Equals(object obj) => obj is SquareMatrix matrix && this == matrix;
   public override int GetHashCode() => Determinant().GetHashCode();
 
-  // 🔹 Реализация паттерна "Прототип" (глубокое копирование)
-  public object Clone() => new SquareMatrix(this);
+
+  public static explicit operator string(SquareMatrix matrix) {
+    string result = "";
+    for (int i = 0; i < matrix._size; ++i) {
+      for (int j = 0; j < matrix._size; ++j)
+        result += $"{matrix._matrix[i, j]} ";
+    }
+
+    return result;
+  }
+
+  public static bool operator true(SquareMatrix matrix) {
+    for (int i = 0; i < matrix.Size; ++i) {
+      for (int j = 0; j < matrix.Size; ++j) {
+        if (matrix._matrix[i, j] != 0) return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static bool operator false(SquareMatrix matrix) {
+    for (int i = 0; i < matrix.Size; ++i) {
+      for (int j = 0; j < matrix.Size; ++j) {
+        if (matrix._matrix[i, j] == 0) return false;
+      }
+    }
+
+    return true;
+  }
 }
